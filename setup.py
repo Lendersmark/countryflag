6797bce5109725af7e5d7653934f3d40a42f7f0b
Lendersmark <78660866+Lendersmark@users.noreply.github.com>
2022-09-27 17:13:29 +0200

    Fix setup.py for pip install

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
-from setuptools import setup
-setup(
+import setuptools
+
+setuptools.setup(
     name='countryflag',
     version='0.1',
-    description='Converts long country names into emoji flags',
-    url='https://github.com/Lendersmark/countryflag',
     author='Lendersmark',
+    description='A Python package for converting country names into emoji flags',
+    url='https://github.com/Lendersmark/countryflag',
+    packages=setuptools.find_packages(),
     license="MIT",
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['countryflag'],
+    package_dir={'':'countryflag/src'},
     install_requires=[
         'emoji-country-flag',
         'country_converter'
      ],
      entry_points={
         'console_scripts': [
-            'countryflag=countryflag.countryflag:run'
+            'countryflag=countryflag:run'
         ]
     }
 )
