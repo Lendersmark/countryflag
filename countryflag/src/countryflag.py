6f089e55c81b5879e19a619129acf77cada08642
Lendersmark <78660866+Lendersmark@users.noreply.github.com>
2022-09-27 22:42:37 +0200

    add description in setup.py

--- countryflag/src/countryflag.py
+++ countryflag/src/countryflag.py
@@ -5,22 +5,23 @@ import sys
 import flag
 import country_converter as coco
 
+
 def run():
     # Initializing variable
-    country_flag=''
+    country_flag = ""
     # Fetches long country name from argument
     if len(sys.argv) <= 1:
-        print ('countryflag: missing country name')
+        print("countryflag: missing country name")
     else:
-        for i in range(1, len(sys.argv)): 
+        for i in range(1, len(sys.argv)):
             # Takes the argument
             country_name = sys.argv[i]
             # Converts long country name to ISO2 code, if needed
-            country_code = coco.convert(names=country_name, to='ISO2')
+            country_code = coco.convert(names=country_name, to="ISO2")
             # Returns emoji flags
-            if i>=2:
+            if i >= 2:
                 # If more than a country, adds a space as separator
                 country_flag += " "
-            country_flag += (flag.flag(country_code))
+            country_flag += flag.flag(country_code)
         print(country_flag)
     sys.exit()
--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-    name='countryflag',
-    version='0.1',
-    author='Lendersmark',
-    description='A Python package for converting country names into emoji flags',
-    url='https://github.com/Lendersmark/countryflag',
+    name="countryflag",
+    version="0.1.1",
+    author="Lendersmark",
+    description="A Python package for converting country names into emoji flags",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lendersmark/countryflag",
     packages=setuptools.find_packages(),
     license="MIT",
     classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    py_modules=['countryflag'],
-    package_dir={'':'countryflag/src'},
-    install_requires=[
-        'emoji-country-flag',
-        'country_converter'
-     ],
-     entry_points={
-        'console_scripts': [
-            'countryflag=countryflag:run'
-        ]
-    }
+    python_requires=">=3.6",
+    py_modules=["countryflag"],
+    package_dir={"": "countryflag/src"},
+    install_requires=["emoji-country-flag", "country_converter"],
+    entry_points={"console_scripts": ["countryflag=countryflag:run"]},
 )
