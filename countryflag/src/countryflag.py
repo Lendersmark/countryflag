f8cf1070cbc99892a72df9980251db9fd102350f
Lendersmark <78660866+Lendersmark@users.noreply.github.com>
2022-09-30 12:44:29 +0200

    Add function to use within Python

--- countryflag/src/countryflag.py
+++ countryflag/src/countryflag.py
@@ -5,24 +5,25 @@ import sys
 import flag
 import country_converter as coco
 
+def getflag(country_name):
+    # initialize variable
+    country_flag = ''
+    for i in range(0, len(country_name)):
+        # convert country name into ISO2 code
+        country_code = coco.convert(names=country_name[i], to='ISO2')
+        # convert ISO2 code into flag
+        if i >= 1:
+                # If more than a country, adds a space as separator
+                country_flag += ' '
+        country_flag += flag.flag(country_code)
+    return country_flag
 
 def run():
-    # Initializing variable
-    country_flag = ""
-    # Fetches long country name from argument
+    # Check if missing arguments
     if len(sys.argv) <= 1:
         print("usage: countryflag [<countryname_1] [<countryname_2>] [<countryname_n>] [...]")
     else:
-        for i in range(1, len(sys.argv)):
-            # Takes the argument
-            country_name = sys.argv[i]
-            # Converts long country name to ISO2 code, if needed
-            country_code = coco.convert(names=country_name, to="ISO2")
-            # Returns emoji flags
-            if i >= 2:
-                # If more than a country, adds a space as separator
-                country_flag += " "
-            country_flag += flag.flag(country_code)
-        print(country_flag)
+        countries = sys.argv[1:]
+        flags=getflag(countries)
+        print(flags)
     sys.exit()
-
