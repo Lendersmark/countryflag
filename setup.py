40f64a3d0f9d0a4cbd8a65a4f3664675d38253da
Lendersmark <78660866+Lendersmark@users.noreply.github.com>
2022-10-10 19:38:26 +0200

    Added tests, reviewed package structure

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="countryflag",
-    version="0.1.1rc3",
+    version="0.1.1rc4",
     author="Lendersmark",
     description="A Python package for converting country names into emoji flags",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Lendersmark/countryflag",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages("src", exclude=["tests"]), # test is excluded
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -23,7 +23,7 @@ setuptools.setup(
     ],
     python_requires=">=3.6",
     py_modules=["countryflag"],
-    package_dir={"": "countryflag/src"},
+    package_dir={"": "src"},
     install_requires=["emoji-country-flag", "country_converter"],
-    entry_points={"console_scripts": ["countryflag=countryflag:run"]},
+    entry_points={"console_scripts": ["countryflag=countryflag:main"]},
 )
