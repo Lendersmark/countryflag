fe4144b191835d18f2c80a36eadb7798f24a4bce
Lendersmark <78660866+Lendersmark@users.noreply.github.com>
2022-10-18 11:56:29 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="countryflag",
-    version="0.1.2b1",
+    version="0.1.2b2",
     author="Lendersmark",
     description="A Python package for converting country names into emoji flags",
     long_description=long_description,
