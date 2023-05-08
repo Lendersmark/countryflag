4a820493d4ee77333a6c2e5dbdef7a2d5c88ea29
Lendersmark <78660866+Lendersmark@users.noreply.github.com>
2023-05-08 17:53:23 +0200

    Update setup.py
    Added a new "classifiers" section with more information about the package.
    
    Added an "extras_require" section to specify development dependencies.
    
    Added a "scripts" section to allow running countryflag.py as a terminal command.
    
    Set zip_safe=False.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="countryflag",
-    version="0.1.2b4",
+    version="0.2.0b1",
     author="Lendersmark",
     description="A Python package for converting country names into emoji flags",
     long_description=long_description,
@@ -20,10 +20,17 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
     ],
     python_requires=">=3.6",
     py_modules=["countryflag"],
     package_dir={"": "src"},
     install_requires=["emoji-country-flag", "country_converter"],
+    extras_require={
+        "dev": ["pytest", "sphinx", "sphinx_rtd_theme"],
+    },
     entry_points={"console_scripts": ["countryflag=countryflag:main"]},
+    scripts=["countryflag.py"],
+    zip_safe=False,
 )
